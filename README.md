1f10165a130701da9ac9922301c58aaa75a8ab81
Rakarake <rak@rakarake.xyz>
2023-11-15 18:07:22 +0100

    Added a test day

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 Ok, to run a specific part, add that to to a `src/main.rs`. The file is
 in the .gitignore, so it is for working on solutions.
+Run with `cargo run --bin aoc`.
+
+A `main.rs` could look like this:
+```rust
+use aoc::aoc2023::day_test;
+
+fn main() {
+    println!("Part 1: {:?}", day_test::part1());
+    println!("Part 2: {:?}", day_test::part2());
+}
+```
+
+When solving a day:
+* Write the solution to `src/<year>/day_<day>.rs`
+* Write the input to `src/<year>/day_<day>_input.txt`
+* Add the solution to that year's table: `src/<year>/mod.rs`
 
--- src/aoc2023/day_test.rs
+++ src/aoc2023/day_test.rs
@@ -1,18 +1,21 @@
-use aoc_table::table_gen::TableGen;
+pub fn part1() -> &'static str {
+    "this is a value"
+    //println!("We do be printing some stuff");
+    //TableGen::new("❄️  Morbin' AOC 2023 Table 🎅 hoho")
+    //    .add(1, rand::random::<f32>, || {
+    //        std::thread::sleep(std::time::Duration::from_secs_f32(4.0));
+    //        rand::random::<f32>()
+    //    })
+    //    .add(8, rand::random::<f32>, || {
+    //        std::thread::sleep(std::time::Duration::from_secs_f32(6.0));
+    //        rand::random::<f32>()
+    //    })
+    //    .add(2, rand::random::<i32>, rand::random::<bool>)
+    //    .add(5, || "Different", || "types!")
+    //    .add_next(|| "Different", || "types!")
+    //    .run();
+}
 
-pub fn part1() {
-    println!("We do be printing some stuff");
-    TableGen::new("❄️  Morbin' AOC 2023 Table 🎅 hoho")
-        .add(1, rand::random::<f32>, || {
-            std::thread::sleep(std::time::Duration::from_secs_f32(4.0));
-            rand::random::<f32>()
-        })
-        .add(8, rand::random::<f32>, || {
-            std::thread::sleep(std::time::Duration::from_secs_f32(6.0));
-            rand::random::<f32>()
-        })
-        .add(2, rand::random::<i32>, rand::random::<bool>)
-        .add(5, || "Different", || "types!")
-        .add_next(|| "Different", || "types!")
-        .run();
+pub fn part2() -> &'static str {
+    "this is another value"
 }
