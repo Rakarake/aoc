ee8af5a3a6032579caa5bfd8025812641672e200
Rakarake <rak@rakarake.xyz>
2023-11-30 19:36:58 +0100

    Part b, wowie, pretty bad though

--- src/aoc2022/day_1.rs
+++ src/aoc2022/day_1.rs
@@ -45,30 +45,48 @@ pub fn part1() -> u32 {
     max
 }
 
-//pub fn part2() -> u32 {
-//    let a: Vec<Vec<u32>> = Vec::new();
-//    let nums = INPUT.lines().map(|line| line.parse::<u32>());
-//    let num_groups = nums.fold(a, |mut acc, maybe_n| {
-//        match maybe_n {
-//            Ok(n) => {
-//                match acc.last_mut() {
-//                    Some(last) => {
-//                        last.push(n)
-//                    }
-//                    None => {
-//                        let new_group: Vec<u32> = vec![n];
-//                        acc.push(new_group);
-//                    }
-//                }
-//            },
-//            Err(_e) => {
-//                acc.push(Vec::new())
-//            }
-//        }
-//        acc
-//    });
-//    let num_sums = num_groups.iter().map(|g| g.iter().fold(0, |sum, x| sum + x));
-//    let mut top_three: [u32; 3] = [0; 3];
-//    let skljskl = num_sums.for_each(|sum| top_three.iter_mut().scan(sum, |_, _| ()));
-//}
+pub fn part2() -> u32 {
+    let a: Vec<Vec<u32>> = Vec::new();
+    let nums = INPUT.lines().map(|line| line.parse::<u32>());
+    let num_groups = nums.fold(a, |mut acc, maybe_n| {
+        match maybe_n {
+            Ok(n) => {
+                match acc.last_mut() {
+                    Some(last) => {
+                        last.push(n)
+                    }
+                    None => {
+                        let new_group: Vec<u32> = vec![n];
+                        acc.push(new_group);
+                    }
+                }
+            },
+            Err(_e) => {
+                acc.push(Vec::new())
+            }
+        }
+        acc
+    });
+    let num_sums = num_groups.iter().map(|g| g.iter().fold(0, |sum, x| sum + x));
+    // Ascending order
+    let mut top_three_tmp: [u32; 3] = [0; 3];
+    for num in num_sums {
+        // Integrate new num
+        if num > top_three_tmp[0] {
+            if num > top_three_tmp[1] {
+                if num > top_three_tmp[2] {
+                    top_three_tmp[0] = top_three_tmp[1];
+                    top_three_tmp[1] = top_three_tmp[2];
+                    top_three_tmp[2] = num;
+                } else {
+                    top_three_tmp[0] = top_three_tmp[1];
+                    top_three_tmp[1] = num;
+                }
+            } else {
+                top_three_tmp[0] = num;
+            }
+        }
+    }
+    top_three_tmp.iter().sum()
+}
 
--- src/aoc2022/mod.rs
+++ src/aoc2022/mod.rs
@@ -4,6 +4,6 @@ use aoc_table::table_gen::TableGen;
 
 pub fn run_all() {
     TableGen::new("Hoho, solutions to problems?")
-        .add_next(day_1::part1, || 0)
+        .add_next(day_1::part1, day_1::part2)
         .run();
 }
