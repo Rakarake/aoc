235ec69367b2f6ebd98eefa22b9a12543a30cff3
Rakarake <rak@rakarake.xyz>
2023-12-01 22:43:52 +0100

    Not right

--- src/aoc2023/day_1.rs
+++ src/aoc2023/day_1.rs
@@ -22,13 +22,13 @@ pub fn part1() -> u32 {
         .sum()
 }
 
+//const letter_words: [&'static str ; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 use nom::combinator::value;
 use nom::branch::alt;
 use nom::multi::many1;
-use nom::character::complete::alpha1;
 use nom::combinator::map;
-
-//const letter_words: [&'static str ; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+use nom::combinator::map_res;
+use nom::character::complete::anychar;
 
 // TODO: make sure they are spelled right
 fn parse_letter_word(i: &str) -> nom::IResult<&str, u32> {
@@ -46,11 +46,14 @@ fn parse_letter_word(i: &str) -> nom::IResult<&str, u32> {
 }
 
 fn parse_letter(i: &str) -> nom::IResult<&str, u32> {
-    alt((parse_letter_word, nom::character::complete::u32))(i)
+    alt((parse_letter_word, map_res(anychar, |c| c.to_digit(10).ok_or("bad soup"))))(i)
 }
 
 fn parse_multiple_letters(i: &str) -> nom::IResult<&str, Vec<Option<u32>>> {
-    many1(alt((map(parse_letter, |l| Some(l)), value(None, alpha1))))(i)
+    many1(alt(
+            (map(parse_letter, |l| Some(l)),
+            value(None, anychar)))
+        )(i)
 }
 
 fn parse_line(line: &str) -> u32 {
@@ -58,10 +61,11 @@ fn parse_line(line: &str) -> u32 {
     let (_, rr) = r.unwrap();
     let letters: Vec<u32> = rr.into_iter().filter_map(|letter| letter).collect();
     // Wow
-    letters.first().unwrap() ^ 10 + letters.last().unwrap()
+    println!("{:?}, {:?}", letters.first().unwrap(), letters.last().unwrap());
+    letters.first().unwrap() * 10 + letters.last().unwrap()
 }
 
 pub fn part2() -> u32 {
-    INPUT.lines().map(|line| parse_line(line) ).sum()
+    INPUT.lines().map(|line| parse_line(line)).sum()
 }
 
