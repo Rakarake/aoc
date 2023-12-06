5dbca2fb1aa21f21350793ff34ccc65db914776e
Rakarake <rak@rakarake.xyz>
2023-12-06 16:01:27 +0100

    not done!

--- src/aoc2023/day6.rs
+++ src/aoc2023/day6.rs
@@ -4,8 +4,19 @@ Time:      7  15   30
 Distance:  9  40  200\
 ";
 
-pub fn part1() -> u32 {
-    0
+const REAL_INPUT_FR: [(u32, u32); 4] = [(53, 250), (91, 1330), (67, 1081), (68, 1025)];
+
+//                                                        higher
+fn get_bounds(tr: f64, d: f64) -> f64 {
+    (
+        ((tr/2f64) + ((tr/2f64).powi(2) - d).sqrt())
+            -
+        ((tr/2f64) - ((tr/2f64).powi(2) - d).sqrt())
+    ).abs()
+}
+
+pub fn part1() -> f64 {
+    REAL_INPUT_FR.into_iter().map(|(t, d)| get_bounds(t as f64, d as f64)).fold(1.0, |o, v| o * v)
 }
 
 pub fn part2() -> u32 {
