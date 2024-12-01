316578af1f032ce5e4eae43824f315e2d9b6ffcd
Rakarake <raka@rakarake.xyz>
2024-12-01 23:52:29 +0100

    Day 1 of 2024!

--- src/aoc2024/day1.rs
+++ src/aoc2024/day1.rs
@@ -1,7 +1,48 @@
-pub fn part1() -> &'static str {
-    "this is a value"
+const TEST_INPUT: &'static str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+const INPUT: &'static str = include_str!("day1.input");
+
+fn parse(i: &str) -> Vec<(i32, i32)> {
+    i
+        .lines()
+        .map(|l| {
+            let ns = l
+                .split_once("   ")
+                .unwrap();
+            (ns.0.parse::<i32>().unwrap(), ns.1.parse::<i32>().unwrap())
+        }
+        ).collect()
 }
 
-pub fn part2() -> &'static str {
-    "this is another value"
+fn the_lists(i: Vec<(i32, i32)>) -> (Vec<i32>, Vec<i32>) {
+    let mut left_list: Vec<i32> = vec![];
+    let mut right_list: Vec<i32> = vec![];
+    for (left, right) in i {
+        left_list.push(left);
+        right_list.push(right);
+    }
+    (left_list, right_list)
+}
+
+pub fn part1() -> i32 {
+    let i = parse(INPUT);
+    let (mut left_list, mut right_list) = the_lists(i);
+    left_list.sort();
+    right_list.sort();
+    use std::iter::zip;
+    zip(left_list.iter(), right_list.iter()).fold(0, |acc, (l, r)| acc + (l - r).abs())
+}
+
+pub fn part2() -> i32 {
+    let i = parse(INPUT);
+    let (mut left_list, mut right_list) = the_lists(i);
+    left_list.iter().fold(0, |acc, n1|
+        acc + n1 * right_list.iter().filter(|n2| n1 == *n2).count() as i32)
 }
